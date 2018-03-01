55a22bd2c281a5999e20edcfa660e73c845e83e5
urimaj <35381652+urimaj@users.noreply.github.com>
2018-03-01 15:46:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,160 +1,51 @@
 var app = new Vue({
   el: '#app',
   data: {
-    musicName: '',
-    result: '',
-  },
-  // created: function() {
-  //   this.GetSongs();
-  // },
-  watch: {
-
+    musicList: [],
+    resultString: '',
+    loading: false,
   },
   methods: {
     GetSongs: function(){
       console.log("get here");
-      this.result = "";
+      this.loading = true;
+      this.resultString = "";
+      this.musicList = [];
       fetch('https://itunes.apple.com/search?term=' + this.musicName).then(response => {
         return response.json();
       }).then(json =>{
-              var results = "";
-              results += '<h1>Search results for ' + this.musicName + ' (total of ' + json.resultCount + ' items)</h1><br>';
-              console.log("DDD");
-              results += "<div class=\"container\"><div class=\"row\">";
-               for (var i = 0; i < json.results.length; i++) {
-
-                var imageurl = json.results[i].artworkUrl100;
-
-                  results += "<div class=\"col-md-4\"><div class=\"colcontainer\">";
-
-                  results += "<div class=\"text-center\">";
-                  results += "<img class=\"rounded\" src="+ imageurl + "></div>";
-
-                  results += "<table class=\"table table-striped\">";
-
-                  var trackname = json.results[i].trackName;
-                  if (trackname.length > 25) {
-                    trackname = trackname.substring(0, 25);
-                    trackname += "...";
-                  }
-                  results += "<tbody><tr><td class=\"boldtd\">Title</td>";
-                  results += "<td>" + trackname + "</td></tr></tbody>";
-
-                 //
-                  results += "<tbody><tr><td class=\"boldtd\">Artist</td>";
-                  var artistname = json.results[i].artistName;
-                  if (artistname.length > 25) {
-                    artistname = artistname.substring(0, 25);
-                    artistname += "...";
-                  }
-                  results += "<td>" + artistname + "</td></tr></tbody>";
-
-
-
-                  results += "<tbody><tr><td class=\"boldtd\">Genre</td>";
-                  results += "<td>" + json.results[i].primaryGenreName + "</td></tr></tbody>";
-
-                  results += "<tbody><tr><td class=\"boldtd\">Release Date</td>";
-                  var releasdate = json.results[i].releaseDate;
-                  releasdate = releasdate.substring(0,10);
-                  results += "<td>" + releasdate + "</td></tr></tbody>";
-                  results += "</table>";
-
-                  results += "<h4 class=\"text-center\"><a href=" + json.results[i].trackViewUrl + " target=\"_blank\">visit itunes</a></h4>";
-
-                  results += "<p class=\"text-center\">Preview</p>";
-                  results += "<div class=\"text-center\">";
-                  results += "<audio controls>";
-                  results += "<source src=\"" + json.results[i].previewUrl + "\" type=\"audio/mpeg\" />";
-                  results += "<source src=\"" + json.results[i].previewUrl + "\" type=\"audio/ogg\" /></audio>";
-
-
-                  results += "</div></div></div>";
-                }
-                console.log(results);
-                results += "</div></div>";
-                this.result =results;
+        this.loading = false;
+          this.resultString = "Search results for " + this.musicName + " (total of " + json.results.length + " items)";
+              for (var i = 0; i < json.results.length; i++) {
+                var data = {
+                  image: json.results[i].artworkUrl100,
+                  trackName : this.truncate(json.results[i].trackName),
+                  artistName: this.truncate(json.results[i].artistName),
+                  GenreName: this.truncate(json.results[i].primaryGenreName),
+                  releaseDate: this.truncateDate(json.results[i].releaseDate),
+                  previewUrl: json.results[i].previewUrl,
+                  itunesUrl: json.results[i].trackViewUrl,
+                };
+                this.musicList.push(data);
+              }
       })
+    },
+    truncate: function(string) {
+      var tempString = string;
+      if (tempString.length > 25) {
+        tempString = tempString.substring(0, 25);
+        tempString += "...";
+      }
+      return tempString;
+    },
+    truncateDate: function(string) {
+      return string.substring(0, 10);
     }
   },
   computed: {
 
   },
+  watch: {
+
+  }
 });
-
-
-// $(document).ready(function() {
-//   var submitButton2 = $("#searchSubmit");
-// });
-//
-// $("#searchSubmit").click(function(e) {
-//   e.preventDefault();
-//   var value = $("#searchInput").val();
-//   console.log(value);
-//   var myurl = "https://itunes.apple.com/search?term=" + value;
-//   $.ajax({
-//     url : myurl,
-//     dataType : "json",
-//     success : function(json) {
-//       console.log(json);
-//       var results = "";
-//       results += '<h1>Search results for ' + value + ' (total of ' + json.resultCount + ' items)</h1><br>';
-//       console.log("DDD");
-//       results += "<div class=\"container\"><div class=\"row\">";
-//        for (var i = 0; i < json.results.length; i++) {
-//
-//         var imageurl = json.results[i].artworkUrl100;
-//
-//           results += "<div class=\"col-md-4\"><div class=\"colcontainer\">";
-//
-//           results += "<div class=\"text-center\">";
-//           results += "<img class=\"rounded\" src="+ imageurl + "></div>";
-//
-//           results += "<table class=\"table table-striped\">";
-//
-//           var trackname = json.results[i].trackName;
-//           if (trackname.length > 25) {
-//             trackname = trackname.substring(0, 25);
-//             trackname += "...";
-//           }
-//           results += "<tbody><tr><td class=\"boldtd\">Title</td>";
-//           results += "<td>" + trackname + "</td></tr></tbody>";
-//
-//          //
-//           results += "<tbody><tr><td class=\"boldtd\">Artist</td>";
-//           var artistname = json.results[i].artistName;
-//           if (artistname.length > 25) {
-//             artistname = artistname.substring(0, 25);
-//             artistname += "...";
-//           }
-//           results += "<td>" + artistname + "</td></tr></tbody>";
-//
-//
-//
-//           results += "<tbody><tr><td class=\"boldtd\">Genre</td>";
-//           results += "<td>" + json.results[i].primaryGenreName + "</td></tr></tbody>";
-//
-//           results += "<tbody><tr><td class=\"boldtd\">Release Date</td>";
-//           var releasdate = json.results[i].releaseDate;
-//           releasdate = releasdate.substring(0,10);
-//           results += "<td>" + releasdate + "</td></tr></tbody>";
-//           results += "</table>";
-//
-//           results += "<h4 class=\"text-center\"><a href=" + json.results[i].trackViewUrl + " target=\"_blank\">visit itunes</a></h4>";
-//
-//           results += "<p class=\"text-center\">Preview</p>";
-//           results += "<div class=\"text-center\">";
-//           results += "<audio controls>";
-//           results += "<source src=\"" + json.results[i].previewUrl + "\" type=\"audio/mpeg\" />";
-//           results += "<source src=\"" + json.results[i].previewUrl + "\" type=\"audio/ogg\" /></audio>";
-//
-//
-//           results += "</div></div></div>";
-//         }
-//         console.log(results);
-//         results += "</div></div>";
-//       $("#searchResults").html(results);
-//       console.log(json);
-//     }
-//   })
-// });
